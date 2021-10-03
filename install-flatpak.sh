c483040d70db557c4648add43fde0305abd9a346
bunsen-mcbean <58019620+bunsen-mcbean@users.noreply.github.com>
2021-10-03 15:58:13 +1100

    Update install-flatpak.sh

--- install-flatpak.sh
+++ install-flatpak.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
 # flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-curl -O https://flathub.org/repo/flathub.flatpakrepo
-sudo flatpak remote-add --from flathub flathub.flatpakrepo
-
-flatpak install    --user -y flathub org.freedesktop.Platform//21.08
-flatpak install    --user -y flathub org.freedesktop.Sdk//21.08
+# curl -O https://flathub.org/repo/flathub.flatpakrepo
+# sudo flatpak remote-add --from flathub flathub.flatpakrepo
 
 if flatpak info --user org.jamovi.jamovi 1>/dev/null 2>/dev/null ; then
     echo "jamovi already installed"
@@ -14,3 +11,6 @@ else
     until flatpak install --user -y https://dl.flathub.org/build-repo/59917/org.jamovi.jamovi.flatpakref; do echo "Trying again"; sleep 2; done
     # flatpak install    --user -y flathub org.jamovi.jamovi
 fi
+
+flatpak install    --user -y flathub org.freedesktop.Platform//21.08
+flatpak install    --user -y flathub org.freedesktop.Sdk//21.08
